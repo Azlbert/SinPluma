c2a3e0d0ddbd163fd56993cf14a5857bc23f2c65
Alberto <agzlbert@gmail.com>
2019-09-27 18:53:50 -0500

    Implemented react-router . NPM Package were updated.

--- reacts/src/components/App.js
+++ reacts/src/components/App.js
@@ -3,16 +3,38 @@ import Template       from './Template';
 import { connect }    from 'react-redux';
 import CssBaseline    from '@material-ui/core/CssBaseline';
 import ThemeProvider  from '@material-ui/styles/ThemeProvider';
-import Cards          from "./Pages/Writer";
+import Writer         from "./Pages/Writer";
+import Cards          from "./Pages/Cards";
+import LoginPage      from "./Pages/Login";
+import { BrowserRouter as Router, Route/* , Link */ } from "react-router-dom";
 
 const App = (props) => {
     return (
         <ThemeProvider theme={props.theme}>
             <CssBaseline/>
-            <Template>
-                <Cards />
-            </Template>
+            <Router>
+                <Route path="/" exact component={LoginPage} />
+                <Route path="/writer/" component={WriterPage} />
+                <Route path="/cards/" component={CardsPage} />
+                <Route path="/login/" component={LoginPage} />
+            </Router>
         </ThemeProvider>
+    );
+};
+
+const WriterPage = () => {
+    return (
+        <Template>
+            <Writer />
+        </Template>
+    );
+};
+
+const CardsPage = () => {
+    return (
+        <Template>
+            <Cards />
+        </Template>
     );
 };
 
